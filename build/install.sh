cc2000b1787e19aae82713d6cd46341e49b4aba5
Bobby Allen <ballen@bobbyallen.me>
2017-08-16 18:25:00 +0100

    Adding build script.

---
+++ build/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Install all required RaspberryPi3 packages
+
+# Create the system user account
+
+# Create all application directories
+
+# Set security permissions
+
+# Install the system service
+
+# Install composer packages
+
+# Rebuild the configuration files
+
+# Restart the system service
