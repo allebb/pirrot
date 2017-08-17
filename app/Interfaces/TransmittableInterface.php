c4d35ba0a0ded054c18872f9f0c7cfa5d2882b4c
Bobby Allen <ballen@bobbyallen.me>
2017-08-17 08:55:49 +0100

    Adds a 'transmittable' interface to be used with the COS and VOX transmit type classes.

---
+++ app/Interfaces/TransmittableInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ballen\Piplex\Interfaces;
+
+/**
+ * Interface TransmittableInterface
+ *
+ * @package Ballen\Piplex\Interfaces
+ */
+interface TransmittableInterface
+{
+    public function transmit();
+}
