732017d4f3990cb5ee040a47798022aaa0cd3433
Bobby Allen <ballen@bobbyallen.me>
2017-08-20 18:26:33 +0100

    Adding two scaffolded classes for the two simplex modes.

---
+++ app/Modes/SimplexCosMode.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ballen\Piplex\Modes;
+
+use Ballen\Piplex\Interfaces\TransmittableInterface;
+
+class SimplexCosMode implements TransmittableInterface
+{
+
+    public function transmit()
+    {
+        // TODO: Implement transmit() method.
+    }
+}
---
+++ app/Modes/SimplexVoxMode.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ballen\Piplex\Modes;
+
+use Ballen\Piplex\Interfaces\TransmittableInterface;
+
+class SimplexVoxMode implements TransmittableInterface
+{
+
+    public function transmit()
+    {
+        // TODO: Implement transmit() method.
+    }
+}
