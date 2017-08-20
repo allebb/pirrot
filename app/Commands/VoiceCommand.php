2f893533d3b708456f63003d604dfa3811537ffc
Bobby Allen <ballen@bobbyallen.me>
2017-08-20 18:29:15 +0100

    Updated code to remove the TestSpeachCommand.

--- app/Commands/VoiceCommand.php
+++ app/Commands/VoiceCommand.php
@@ -31,17 +31,7 @@ class VoiceCommand extends AudioBaseCommand implements CommandInterface
      */
     public function handle()
     {
-        while (true) {
-            if ($this->config->get('auto_ident')) {
-                $this->audioService->ident(
-                    $this->config->get('callsign'),
-                    $this->config->get('pl_tone', null),
-                    $this->config->get('ident_time'),
-                    $this->config->get('ident_morse')
-                );
-            }
-            sleep($this->config->get('ident_interval'));
-        }
+        // Split the config setting on '_' and then check a class exists...
     }
 
 
