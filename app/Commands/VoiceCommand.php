45034434799659d74825f874192f2e86664024c3
Bobby Allen <ballen@bobbyallen.me>
2017-08-21 09:51:54 +0100

    The VOX and COS loops are now part of the main VoiceCommand class and are working well.

--- app/Commands/VoiceCommand.php
+++ app/Commands/VoiceCommand.php
@@ -5,6 +5,7 @@ namespace Ballen\Piplex\Commands;
 use Ballen\Clip\Traits\RecievesArgumentsTrait;
 use Ballen\Clip\Interfaces\CommandInterface;
 use Ballen\Clip\Utilities\ArgumentsParser;
+use Ballen\Piplex\Interfaces\RepeatableInterface;
 
 /**
  * Class VoiceCommand
@@ -17,6 +18,13 @@ class VoiceCommand extends AudioBaseCommand implements CommandInterface
     use RecievesArgumentsTrait;
 
     /**
+     * The TX/RX mode for voice comms.
+     *
+     * @var string
+     */
+    private $mode;
+
+    /**
      * VoiceCommand constructor.
      *
      * @param ArgumentsParser $argv
@@ -24,6 +32,9 @@ class VoiceCommand extends AudioBaseCommand implements CommandInterface
     public function __construct(ArgumentsParser $argv)
     {
         parent::__construct($argv);
+
+        // Sets the transmit/recieve mode
+        $this->mode = ucwords($this->config->get('transmit_mode'));
     }
 
     /**
@@ -31,7 +42,23 @@ class VoiceCommand extends AudioBaseCommand implements CommandInterface
      */
     public function handle()
     {
-        // Split the config setting on '_' and then check a class exists...
+        $modeHandler = "main{$this->mode}";
+        var_dump($modeHandler);
+        if (method_exists($this, $modeHandler)) {
+            return $this->{$modeHandler}();
+        }
+        $this->writeln("RX/TX mode ({$this->mode}) now supported!");
+        $this->exitWithError();
+    }
+
+    private function mainVox()
+    {
+        $this->writeln('Running VOX main loop!');
+    }
+
+    private function mainCos()
+    {
+        $this->writeln('Running COS main loop!');
     }
 
 
