9b4596e33a7881d3b427a46d800c350a74ebc9fa
Bobby Allen <ballen@bobbyallen.me>
2017-08-24 15:53:45 +0100

    Adding new changes in order to test the IoInterupt functionality

--- app/Commands/IoInteruptCommand.php
+++ app/Commands/IoInteruptCommand.php
@@ -62,19 +62,59 @@ class IoInteruptCommand extends PiplexBaseCommand implements CommandInterface
             $this->exitWithError();
         }
 
-        $switch = $this->gpio->pin(23, GPIO::IN);
+        // COS (Carrier signal relay)
+        $cos = $this->gpio->pin(18, GPIO::IN);
+
+        // PTT (Transmit relay)
+        $ptt = $this->gpio->pin(23, GPIO::OUT);
+
+        // Device LEDS
+        $power = $this->gpio->pin(17, GPIO::OUT);
+        $rx = $this->gpio->pin(27, GPIO::OUT);
+        $tx = $this->gpio->pin(22, GPIO::OUT);
+
+        // Turn the power LED on...
+        $power->setValue(GPIO::HIGH);
 
         // Output some debug information...
         $this->writeln('Starting test IO runner...');
 
+        // Record how long the communication was for (then transmit that long!)
+        $cosTimer = 0;
 
-        while (true) {
-            $val = $switch->getValue();
-            if ($val != $this->previousState) {
-                $this->previousState = $val;
-                $this->writeln('Switch state changed to ' . $val);
+        try {
+            while (true) {
+
+                // Get the COS value (are we recieving?)
+                $val = $cos->getValue();
+
+                if ($cosTimer > 0 && $val == GPIO::LOW) {
+                    // We're simply playing back the message and then resetting the counter and returning to the start...
+                    $tx->setValue(GPIO::HIGH);
+                    usleep(50000 * $cosTimer);
+                    $cosTimer = 0;
+                    $tx->setValue(GPIO::LOW);
+                    continue; // Restart the loop again.
+                }
+
+                // If the COS is high we turn on the RX led otherwise we turn if off!
+                if ($val == GPIO::HIGH) {
+                    $cosTimer++; // Count the comminication time (we'll transmit for this long!)
+                    $rx->setValue(GPIO::HIGH);
+                } else {
+                    $rx->setValue(GPIO::LOW);
+                }
+
+                // If the state has changed, we'll output it to the display!
+                if ($val != $this->previousState) {
+                    $this->previousState = $val;
+                    $this->writeln($val);
+                }
+                usleep(50000); // Half a second to wait for new input...
             }
-            usleep(50000); // Half a second
+        } finally {
+            // The try/catch loop helps us do stuff if we quit out of the loop!
+            $power->setValue(GPIO::LOW);
         }
         $this->exitWithSuccess();
     }
