1626ec0ba07c7758119bf7a115fdc6be5872fcd5
Bobby Allen <bobbyallen.uk@gmail.com>
2017-08-31 16:22:45 +0100

    Minor updates to ensure files are deleted as required.

--- build/scripts/uninstall.sh
+++ build/scripts/uninstall.sh
@@ -3,11 +3,11 @@
 echo "Uninstalling Pirrot..."
 
 echo " - Deleting Pirrot Configuration file..."
-rm /etc/pirrot.conf
+rm -f /etc/pirrot.conf
 echo " - Stopping Pirrot daemon..."
-/etc/init.d/pirrot stop
+sudo /etc/init.d/pirrot stop
 echo " - Removing the Pirrot daemon..."
-rm /etc/init.d/pirrot
+rm -f /etc/init.d/pirrot
 echo " - Removing the Pirrot application..."
 rm -Rf /opt/pirrot
 echo " - Removing Composer..."
