498e5e422a2dd7c2b16185cbb0e00b9fdb0f002f
Bobby Allen <bobbyallen.uk@gmail.com>
2017-08-31 16:26:06 +0100

    Sudo updates

--- build/scripts/uninstall.sh
+++ build/scripts/uninstall.sh
@@ -3,15 +3,15 @@
 echo "Uninstalling Pirrot..."
 
 echo " - Deleting Pirrot Configuration file..."
-rm -f /etc/pirrot.conf
+sudo rm -f /etc/pirrot.conf
 echo " - Stopping Pirrot daemon..."
 sudo /etc/init.d/pirrot stop
 echo " - Removing the Pirrot daemon..."
-rm -f /etc/init.d/pirrot
+sudo rm -f /etc/init.d/pirrot
 echo " - Removing the Pirrot application..."
-rm -Rf /opt/pirrot
+sudo rm -Rf /opt/pirrot
 echo " - Removing Composer..."
-rm -f /usr/bin/composer
+sudo rm -f /usr/bin/composer
 echo ""
 echo "Done!"
 echo ""
