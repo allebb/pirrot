02bfe91159ebfc231682c53768ba9a06dbd4e71c
Bobby Allen <ballen@bobbyallen.me>
2017-09-01 12:52:56 +0100

    Makefile and uninstall have been updated in order to correctly remove Pirrot from the device.

--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@ install:
 	@/opt/pirrot/build/scripts/install.sh
 
 uninstall:
-	@/opt/pirrot/build/scripts/uninstall.sh
+	@cp /opt/pirrot/build/scripts/uninstall.sh /tmp/pirrot-uninstall
+	@echo "Run 'sudo /tmp/pirrot-uninstall' to remove Pirrot"
 
 clean:
 	rm -f /opt/pirrot/storage/input/*.ogg
--- build/scripts/uninstall.sh
+++ build/scripts/uninstall.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
-
+cd /tmp
 echo "Uninstalling Pirrot..."
 echo ""
-echo " - Re-enabling on-board audio..."
-sudo sed -i "s|options snd-usb-audio index=-2|#options snd-usb-audio index=-2|" /lib/modprobe.d/aliases.conf
-sudo sed -i "s/blacklist snd_bcm2835/d" /etc/modprobe.d/raspi-blacklist.conf
-echo " - Deleting Pirrot Configuration file..."
-sudo rm -f /etc/pirrot.conf
 echo " - Stopping Pirrot daemon..."
 sudo /etc/init.d/pirrot stop
+sleep 3
+echo " - Re-enabling on-board audio..."
+sudo sed -i "s|#options snd-usb-audio index=-2|options snd-usb-audio index=-2|" /lib/modprobe.d/aliases.conf
+sudo truncate -s 0 /etc/modprobe.d/raspi-blacklist.conf
+echo " - Deleting Pirrot Configuration file..."
+sudo rm -f /etc/pirrot.conf
 echo " - Removing the Pirrot daemon..."
 sudo rm -f /etc/init.d/pirrot
 echo " - Removing the Pirrot application..."
