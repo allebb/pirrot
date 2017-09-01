e5144dc9b6a31649b9f64ef21ca1659aad876603
Bobby Allen <bobbyallen.uk@gmail.com>
2017-09-01 14:25:44 +0100

    Final fixes for the Pirrot uninstall script.

--- build/scripts/uninstall.sh
+++ build/scripts/uninstall.sh
@@ -2,9 +2,6 @@
 cd /tmp
 echo "Uninstalling Pirrot..."
 echo ""
-echo " - Stopping Pirrot daemon..."
-sudo /etc/init.d/pirrot stop
-sleep 3
 echo " - Re-enabling on-board audio..."
 sudo sed -i "s|#options snd-usb-audio index=-2|options snd-usb-audio index=-2|" /lib/modprobe.d/aliases.conf
 sudo truncate -s 0 /etc/modprobe.d/raspi-blacklist.conf
@@ -17,6 +14,9 @@ sudo rm -Rf /opt/pirrot
 echo " - Removing Composer..."
 sudo rm -f /usr/bin/composer
 echo ""
+#echo " - Stopping Pirrot daemon..."
+#sudo /etc/init.d/pirrot stop
+#sleep 3
 echo "Done!"
 echo ""
 # Finished!
