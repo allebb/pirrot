2488ce5e8906539befcde5df8bded144f3f7bf90
Bobby Allen <ballen@bobbyallen.me>
2020-05-05 17:32:01 +0100

    Adds detection and output of OS build and hardware version information, this is now outputted to the version output command for debug/issue logging purposes.

--- app/Commands/VersionCommand.php
+++ app/Commands/VersionCommand.php
@@ -15,9 +15,31 @@ class VersionCommand extends BaseCommand implements CommandInterface
     use RecievesArgumentsTrait;
 
     /**
-     * The Pirrot release/version number
+     * The Pirrot release/version number.
      */
     const PIRROT_VERSION = "1.5.0";
+
+    /**
+     * The file where the OS information can be retrieved from.
+     */
+    const OS_BUILD_INFO_FILE = "/etc/os-release";
+
+    /**
+     * The file where the Raspberry Pi Hardware version can be retrieved from.
+     */
+    const HARDWARE_VERSION_FILE = "/sys/firmware/devicetree/base/model";
+
+    /**
+     * The detected operating system version that Pirrot is running on.
+     * @var string
+     */
+    private $raspbainVersion = "**not detected**";
+
+    /**
+     * The detected Raspberry Pi hardware version that Pirrot is running on.
+     * @var string
+     */
+    private $hardwareVersion = "**not detected**";
 
     /**
      * Handle the command.
@@ -25,8 +47,44 @@ class VersionCommand extends BaseCommand implements CommandInterface
      */
     public function handle()
     {
+        $this->detectRaspbianVersion();
+        $this->detectHardwareVersion();
         $this->writeln('Pirrot v' . self::PIRROT_VERSION);
-        $this->writeln('  compiler version ' . phpversion() . ' (' . php_uname('v') . ') on ' . php_uname('m'));
+        $this->writeln('    - HW version: ' . $this->hardwareVersion);
+        $this->writeln('    - OS version: ' . $this->raspbainVersion);
+        $this->writeln('    - Compiler version: ' . phpversion() . ' (' . php_uname('v') . ') on ' . php_uname('m'));
         $this->exitWithSuccess();
+    }
+
+    /**
+     * Detects the Raspberry Pi hardware version.
+     */
+    private function detectHardwareVersion()
+    {
+        if (!file_exists('/sys/firmware/devicetree/base/model')) {
+            return;
+        }
+        $this->hardwareVersion = file_get_contents(self::HARDWARE_VERSION_FILE);
+    }
+
+    /**
+     * Detects the operating system version.
+     * @return void
+     */
+    private function detectRaspbianVersion()
+    {
+        $osMatches = [];
+        $versionMatches = [];
+        if (!file_exists(self::OS_BUILD_INFO_FILE)) {
+            return;
+        }
+        $osReleaseFile = file_get_contents(self::OS_BUILD_INFO_FILE);
+        preg_match("/\bID=(.+)/i", $osReleaseFile, $osMatches);
+        preg_match("/\bVERSION=(.+)/i", $osReleaseFile, $versionMatches);
+
+        if (isset($versionMatches[1])) {
+            $this->raspbainVersion = trim(ucwords($osMatches[1]), '"') . " " . trim($versionMatches[1], '"');
+        }
+
     }
 }
