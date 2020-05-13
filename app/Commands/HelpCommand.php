b0260302b2befced899d2b9df2c6063e67078548
Bobby Allen <ballen@bobbyallen.me>
2020-05-13 12:35:17 +0100

    Updates and adds new CLI command to set (or reset) the Pirrot web interface admin password.

--- app/Commands/HelpCommand.php
+++ app/Commands/HelpCommand.php
@@ -27,10 +27,10 @@ class HelpCommand extends BaseCommand implements CommandInterface
         $this->writeln();
         $this->writeln('Commands:');
         $this->writeln('  help       - This information screen');
-        //$this->writeln('  ident      - Repeater Ident service daemon.');
-        //$this->writeln('  voice      - Repeater Voice I/O service daemon.');
         $this->writeln('  update     - Runs the software updater.');
         $this->writeln('  version    - Output version and runtime information.');
+        $this->writeln('');
+        $this->writeln('  setwebpwd  - Set new web interface admin password.');
         $this->writeln();
         $this->exitWithSuccess();
     }
---
+++ app/Commands/SetAdminPwdCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Ballen\Pirrot\Commands;
+
+use Ballen\Clip\Traits\RecievesArgumentsTrait;
+use Ballen\Clip\Interfaces\CommandInterface;
+
+/**
+ * Class SetAdminPwdCommand
+ *
+ * @package Ballen\Pirrot\Commands
+ */
+class SetAdminPwdCommand extends BaseCommand implements CommandInterface
+{
+    use RecievesArgumentsTrait;
+
+    /**
+     * Handle the command.
+     * @return void
+     */
+    public function handle()
+    {
+
+        $password = $this->arguments()->getOption('password', null);
+
+        if (!$password) {
+            $this->writeln('No password specified, please set with: --password="YourNewPasswordHere!"');
+            $this->exitWithError();
+        }
+
+        $securedPassword = password_hash($password, PASSWORD_BCRYPT);
+
+        if (!file_put_contents('/opt/pirrot/storage/password.vault', $securedPassword)) {
+            $this->writeln('Error, could not store updated password!');
+            $this->exitWithError();
+        }
+
+        $this->writeln("Password has been updated successfully!");
+        $this->exitWithSuccess();
+    }
+}
