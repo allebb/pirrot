0a0f9c8c9338b13f1810d447be8847405dfef047
Bobby Allen <ballen@bobbyallen.me>
2020-05-13 14:20:07 +0100

    Renamed route to ensure it doesn't clash with the public/recordings directory symlink

--- web/routes/web.php
+++ web/routes/web.php
@@ -7,7 +7,7 @@ $router->get('/', function () {
 $router->group(['middleware' => 'auth.pirrot'], function () use ($router) {
     $router->get('/dashboard', 'DashboardController@showDashboardPage');
     $router->get('/dashboard/stats', 'DashboardController@ajaxGetDashboardStats');
-    $router->get('/recordings', 'RecordingsController@showRecordingsPage');
+    $router->get('/audio-recordings', 'RecordingsController@showRecordingsPage');
 });
 
 
