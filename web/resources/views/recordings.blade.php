9805c18422eadfa9a826ca657fcf46b2eb5cba2b
Bobby Allen <ballen@bobbyallen.me>
2020-05-13 16:35:25 +0100

    Updates the temporary recordings list with some other values.

--- web/resources/views/recordings.blade.php
+++ web/resources/views/recordings.blade.php
@@ -7,9 +7,8 @@
         </tr>
         @foreach($recordings as $recording)
             <tr>
-                <td>{{ rtrim($recording->getFilename(), '.'.$recording->getExtension()) }}
-                    - {{ $recording->getExtension() }} Size: {{ ($recording->getSize() * 1024) }}KB
-                    Type: {{ $recording->getType() }} Created at: {{ $recording->getCTime() }} [<a href="{{ route('download-recording', ['filename' => rtrim($recording->getFilename(), '.'.$recording->getExtension())]) }}">Download</a>]</td>
+                <td>{{ rtrim($recording->getFilename(), '.'.$recording->getExtension()) }}.{{ $recording->getExtension() }} ({{ ($recording->getSize() / 1024) }}KB)
+                    Created at: {{ date('H:i:s jS M Y', $recording->getMTime()) }} [<a href="{{ route('download-recording', ['filename' => rtrim($recording->getFilename(), '.'.$recording->getExtension())]) }}">Download</a>] [<a href="{{ route('delete-recording', ['filename' => rtrim($recording->getFilename(), '.'.$recording->getExtension())]) }}">Delete</a>]</td>
             </tr>
         @endforeach
 
