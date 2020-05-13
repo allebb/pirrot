13689808d45c20a90d67cf4f3a041641006bdb46
Bobby Allen <ballen@bobbyallen.me>
2020-05-13 16:48:03 +0100

    Added some notices to the recordings view, these are to warn/notify the user about the recording functionality disabled state as well as a notice about automatic purging of recordings (if this is set in the user's settings).

--- web/resources/views/recordings.blade.php
+++ web/resources/views/recordings.blade.php
@@ -1,5 +1,9 @@
 <h1>File recordings</h1>
 
+@if(!app('pirrot.config')->store_recordings)
+    <p>If you wish to listen back or download recordings, you must first enable the <em>store_recordings</em> option in your configuration as at present it is disabled.</p>
+@endif
+
 @if($recordings->count() > 0)
     <table>
         <tr>
@@ -7,12 +11,20 @@
         </tr>
         @foreach($recordings as $recording)
             <tr>
-                <td>{{ rtrim($recording->getFilename(), '.'.$recording->getExtension()) }}.{{ $recording->getExtension() }} ({{ ($recording->getSize() / 1024) }}KB)
-                    Created at: {{ date('H:i:s jS M Y', $recording->getMTime()) }} [<a href="{{ route('download-recording', ['filename' => rtrim($recording->getFilename(), '.'.$recording->getExtension())]) }}">Download</a>] [<a href="{{ route('delete-recording', ['filename' => rtrim($recording->getFilename(), '.'.$recording->getExtension())]) }}">Delete</a>]</td>
+                <td>{{ rtrim($recording->getFilename(), '.'.$recording->getExtension()) }}
+                    .{{ $recording->getExtension() }} ({{ ($recording->getSize() / 1024) }}KB)
+                    Created at: {{ date('H:i:s jS M Y', $recording->getMTime()) }} [<a
+                        href="{{ route('download-recording', ['filename' => rtrim($recording->getFilename(), '.'.$recording->getExtension())]) }}">Download</a>]
+                    [<a href="{{ route('delete-recording', ['filename' => rtrim($recording->getFilename(), '.'.$recording->getExtension())]) }}">Delete</a>]
+                </td>
             </tr>
         @endforeach
 
     </table>
 @else
     <p>No recordings found.</p>
+@endif
+
+@if(!app('pirrot.config')->purge_recording_after > 0)
+    <p>Based on your current configuration, these recordings are automatically purged after {{ number_format(app('pirrot.config')->purge_recording_after) }} days. </p>
 @endif
