56d6098e8fc807fa037346ee5097e8d44f800bb8
Bobby Allen <ballen@bobbyallen.me>
2020-05-16 08:59:39 +0100

    Refactored version debug system info retrieval to a new SystemInfoService class, added new --debug flag to dump all system info to JSON format when running "pirrot version --debug" which will also be used and cached for the admin web interface dashboard stats when the daemon starts.

--- app/Commands/VersionCommand.php
+++ app/Commands/VersionCommand.php
@@ -4,6 +4,7 @@ namespace Ballen\Pirrot\Commands;
 
 use Ballen\Clip\Traits\RecievesArgumentsTrait;
 use Ballen\Clip\Interfaces\CommandInterface;
+use Ballen\Pirrot\Services\SystemInfoService;
 
 /**
  * Class HelpCommand
@@ -15,102 +16,39 @@ class VersionCommand extends BaseCommand implements CommandInterface
     use RecievesArgumentsTrait;
 
     /**
-     * The file where the OS information can be retrieved from.
-     */
-    const OS_BUILD_INFO_FILE = "/etc/os-release";
-
-    /**
-     * The file where the Raspberry Pi Hardware version can be retrieved from.
-     */
-    const HARDWARE_VERSION_FILE = "/sys/firmware/devicetree/base/model";
-
-    /**
-     * The detected operating system version that Pirrot is running on.
-     * @var string
-     */
-    private $raspbainVersion = "**not detected**";
-
-    /**
-     * The detected Raspberry Pi hardware version that Pirrot is running on.
-     * @var string
-     */
-    private $hardwareVersion = "**not detected**";
-
-    /**
-     * The Pirrot version (release) number.
-     * @var string
-     */
-    private $pirrotVersion = "**not detected**";
-
-    /**
      * Handle the command.
      * @return void
      */
     public function handle()
     {
-        $this->detectPirrotVersion();
-        $this->detectRaspbianVersion();
-        $this->detectHardwareVersion();
-        
+
+        $systemInfo = new SystemInfoService();
+
+        $systemInfo->detect();
+
+        if ($this->arguments()->isFlagSet('debug')) {
+            $systemInfo = new SystemInfoService();
+            $systemInfo->detect();
+            $this->write((string)$systemInfo);
+            $this->exitWithSuccess();
+        }
+
         if ($this->arguments()->isFlagSet('json')) {
             $this->write(json_encode([
-                'version' => $this->pirrotVersion,
-                'hw_version' => $this->hardwareVersion,
-                'os_version' => $this->raspbainVersion,
-                'compiler_version' => phpversion() . ' (' . php_uname('v') . ') on ' . php_uname('m'),
+                'version' => $systemInfo->pirrotVersion,
+                'hw_version' => $systemInfo->hardwareModel,
+                'os_version' => $systemInfo->raspbainVersion,
+                'compiler_version' => $systemInfo->phpVersion . ' on ' . $systemInfo->hardwareCpuArch,
             ]));
             $this->exitWithSuccess();
         }
 
-        $this->writeln('Pirrot v' . $this->pirrotVersion);
-        $this->writeln('    - HW version: ' . $this->hardwareVersion);
-        $this->writeln('    - OS version: ' . $this->raspbainVersion);
-        $this->writeln('    - Compiler version: ' . phpversion() . ' (' . php_uname('v') . ') on ' . php_uname('m'));
+        $this->writeln('Pirrot v' . $systemInfo->pirrotVersion);
+        $this->writeln('    - HW version: ' . $systemInfo->hardwareModel);
+        $this->writeln('    - OS version: ' . $systemInfo->raspbainVersion);
+        $this->writeln('    - Compiler version: ' . $systemInfo->phpVersion . ' on ' . $systemInfo->hardwareCpuArch);
         $this->exitWithSuccess();
     }
 
-    /**
-     * Detects the Raspberry Pi hardware version.
-     * @return void
-     */
-    private function detectHardwareVersion()
-    {
-        if (!file_exists('/sys/firmware/devicetree/base/model')) {
-            return;
-        }
-        $this->hardwareVersion = file_get_contents(self::HARDWARE_VERSION_FILE);
-    }
 
-    /**
-     * Detects the operating system version.
-     * @return void
-     */
-    private function detectRaspbianVersion()
-    {
-        $osMatches = [];
-        $versionMatches = [];
-        if (!file_exists(self::OS_BUILD_INFO_FILE)) {
-            return;
-        }
-        $osReleaseFile = file_get_contents(self::OS_BUILD_INFO_FILE);
-        preg_match("/\bID=(.+)/i", $osReleaseFile, $osMatches);
-        preg_match("/\bVERSION=(.+)/i", $osReleaseFile, $versionMatches);
-
-        if (isset($versionMatches[1])) {
-            $this->raspbainVersion = trim(ucwords($osMatches[1]), '"') . " " . trim($versionMatches[1], '"');
-        }
-    }
-
-    /**
-     * Detects the Pirrot release version.
-     * @return void
-     */
-    private function detectPirrotVersion()
-    {
-        $pirrotVersionInfo = '/opt/pirrot/VERSION';
-        if (!file_exists($pirrotVersionInfo)) {
-            return;
-        }
-        $this->pirrotVersion = trim(file_get_contents($pirrotVersionInfo));
-    }
 }
---
+++ app/Services/SystemInfoService.php
@@ -0,0 +1,250 @@
+<?php
+
+
+namespace Ballen\Pirrot\Services;
+
+
+class SystemInfoService
+{
+
+    /**
+     * Display text for in-detectable system information data.
+     */
+    const NOT_DETECTED = "**not detected**";
+
+    /**
+     * The file where the OS information can be retrieved from.
+     */
+    const OS_BUILD_INFO_FILE = "/etc/os-release";
+
+    /**
+     * The file where the Raspberry Pi Hardware version can be retrieved from.
+     */
+    const HARDWARE_VERSION_FILE = "/sys/firmware/devicetree/base/model";
+
+    /**
+     * The system hostname.
+     * @var string
+     */
+    public $hostname = self::NOT_DETECTED;
+
+    /**
+     * The detected Raspberry Pi hardware version that Pirrot is running on.
+     * @var string
+     */
+    public $hardwareModel = self::NOT_DETECTED;
+
+    /**
+     * The detected Raspberry Pi hardware serial number that Pirrot is running on.
+     * @var string
+     */
+    public $hardwareSerial = self::NOT_DETECTED;
+
+    /**
+     * The CPU architecture.
+     * @var string
+     */
+    public $hardwareCpuArch = self::NOT_DETECTED;
+
+    /**
+     * The CPU (core) count.
+     * @var int
+     */
+    public $hardwareCpuCount = self::NOT_DETECTED;
+
+    /**
+     * The CPU speed (max frequency)
+     * @var int
+     */
+    public $hardwareCpuFrequency = self::NOT_DETECTED;
+
+    /**
+     * The detected operating system version that Pirrot is running on.
+     * @var string
+     */
+    public $raspbainVersion = self::NOT_DETECTED;
+
+    /**
+     * The detected operating system kernel version.
+     * @var string
+     */
+    public $kernelVersion = self::NOT_DETECTED;
+
+    /**
+     * The Pirrot version (release) number.
+     * @var string
+     */
+    public $pirrotVersion = self::NOT_DETECTED;
+
+    /**
+     * The PHP version that Pirrot is compiled (running) on.
+     * @var string
+     */
+    public $phpVersion = self::NOT_DETECTED;
+
+    /**
+     * Detects if gpsd is installed (and we can therefore access GPS data)
+     * @var bool
+     */
+    public $hasGpsConfigured = false;
+
+
+    /**
+     * Detect and cache all.
+     * @return void
+     */
+    public function detect()
+    {
+        $this->detectHostname();
+        $this->detectPhpVersion();
+        $this->detectKernelVersion();
+        $this->detectHardwareModel();
+        $this->detectHardwareSerial();
+        $this->detectHardwareCpuArch();
+        $this->detectCpuCoreCount();
+        $this->detectCpuFrequency();
+        $this->detectGpsHardware();
+        $this->detectRaspbianVersion();
+        $this->detectPirrotVersion();
+    }
+
+
+    /**
+     * Detects the system hostname.
+     * @return void
+     */
+    protected function detectHostname()
+    {
+        if ($hostname = gethostname()) {
+            $this->hostname = $hostname;
+        }
+    }
+
+    /**
+     * Detects the Raspberry Pi hardware version.
+     * @return void
+     */
+    protected function detectHardwareModel()
+    {
+        if (!file_exists('/sys/firmware/devicetree/base/model')) {
+            return;
+        }
+        $this->hardwareModel = file_get_contents(self::HARDWARE_VERSION_FILE);
+    }
+
+    /**
+     * Detects the operating system version.
+     * @return void
+     */
+    protected function detectRaspbianVersion()
+    {
+        $osMatches = [];
+        $versionMatches = [];
+        if (!file_exists(self::OS_BUILD_INFO_FILE)) {
+            return;
+        }
+        $osReleaseFile = file_get_contents(self::OS_BUILD_INFO_FILE);
+        preg_match("/\bID=(.+)/i", $osReleaseFile, $osMatches);
+        preg_match("/\bVERSION=(.+)/i", $osReleaseFile, $versionMatches);
+
+        if (isset($versionMatches[1])) {
+            $this->raspbainVersion = trim(ucwords($osMatches[1]), '"') . " " . trim($versionMatches[1], '"');
+        }
+    }
+
+    /**
+     * Detects the Pirrot release version.
+     * @return void
+     */
+    protected function detectPirrotVersion()
+    {
+        $pirrotVersionInfo = '/opt/pirrot/VERSION';
+        if (!file_exists($pirrotVersionInfo)) {
+            return;
+        }
+        $this->pirrotVersion = trim(file_get_contents($pirrotVersionInfo));
+    }
+
+    /**
+     * Detects the Raspberry Pi Hardware Serial number
+     * @return void
+     */
+    protected function detectHardwareSerial()
+    {
+        if (!file_exists('/proc/cpuinfo')) {
+            return;
+        }
+        $this->hardwareSerial = trim(system("cat /proc/cpuinfo |grep Serial|cut -d' ' -f2"));
+
+    }
+
+    /**
+     * Detects if GPSD has been setup on this system
+     * @return void
+     */
+    protected function detectGpsHardware()
+    {
+        if (!file_exists('/etc/default/gpsd')) {
+            $this->hasGpsConfigured = false;
+        }
+        $this->hasGpsConfigured = true;
+    }
+
+    /**
+     * The PHP version that Pirrot is compiled (running) on.
+     * @return void
+     */
+    protected function detectPhpVersion()
+    {
+        $this->phpVersion = phpversion();
+    }
+
+    /**
+     * The operating system kernel version.
+     * @return void
+     */
+    protected function detectKernelVersion()
+    {
+        $this->kernelVersion = php_uname('r');
+    }
+
+    /**
+     * The CPU architecture.
+     * @return void
+     */
+    protected function detectHardwareCpuArch()
+    {
+        $this->hardwareCpuArch = php_uname('m');
+    }
+
+    /**
+     * Detect the number of CPUs (cores) for this system.
+     * @return void
+     */
+    protected function detectCpuCoreCount()
+    {
+        $cpuCount = system("lscpu | grep \"CPU(s):\" | cut -d : -f2");
+
+        $count = trim($cpuCount);
+
+        if (is_numeric($count)) {
+            $this->hardwareCpuCount = (int)$count;
+        }
+    }
+
+    /**
+     * Detect the CPU Speed (max frequency) for this system.
+     * @return void
+     */
+    protected function detectCpuFrequency()
+    {
+        $cpuFreq = system("lscpu | grep \"CPU max MHz:\" | cut -d : -f2");
+
+        $cpuFreq = trim($cpuFreq);
+
+        if (is_numeric($cpuFreq)) {
+            $this->hardwareCpuFrequency = (int)$cpuFreq;
+        }
+    }
+    
+}
