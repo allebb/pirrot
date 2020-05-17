a0bef2d5a849dddc415da2426b0c57849b814a8e
Bobby Allen <ballen@bobbyallen.me>
2020-05-17 16:42:53 +0100

    CPU reader improvements.

--- web/app/Services/SystemResourceService.php
+++ web/app/Services/SystemResourceService.php
@@ -77,7 +77,7 @@ class SystemResourceService
 
     public function getCpuUsage(): int
     {
-        $cpuUsage = trim(shell_exec("awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1) \"%\"; }' <(grep 'cpu ' /proc/stat) <(sleep 0.5;grep 'cpu ' /proc/stat)"));
+        $cpuUsage = trim(shell_exec("awk '{u=$2+$4; t=$2+$4+$5; if (NR==1){u1=u; t1=t;} else print ($2+$4-u1) * 100 / (t-t1) "%"; }' <(grep 'cpu ' /proc/stat) <(sleep 0.5;grep 'cpu ' /proc/stat)"));
         return rtrim($cpuUsage, "%");
     }
 
