d6429ae63ebe981285cc881bdbe12a65762c90e4
Bobby Allen <ballen@bobbyallen.me>
2020-05-19 09:02:13 +0100

    Fixes namespace issue on the Setting DTO following some refactoring yesterday.

--- web/app/Http/Controllers/SettingsController.php
+++ web/app/Http/Controllers/SettingsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Services\ConfManagerService;
-use App\Services\Setting;
+use App\Services\DTO\Setting;
 use Illuminate\Http\Request;
 
 class SettingsController extends Controller
--- web/app/Services/DTO/Setting.php
+++ web/app/Services/DTO/Setting.php
@@ -1,7 +1,7 @@
 <?php
 
 
-namespace App\Services;
+namespace App\Services\DTO;
 
 
 class Setting
