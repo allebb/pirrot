ce9f0db30cccb3cfa125f7554f3cbb7aec07ede1
Bobby Allen <ballen@bobbyallen.me>
2020-05-21 11:09:10 +0100

    Tripwire Command has been updated to dispatch the HTTP payload.

--- app/Commands/TripwireCommand.php
+++ app/Commands/TripwireCommand.php
@@ -14,23 +14,71 @@ class TripwireCommand extends BaseCommand implements CommandInterface
 {
     use RecievesArgumentsTrait;
 
+    protected const INTERVAL_CACHE_FILE = '/storage/tripwire.cache';
+
     /**
      * Handle the command.
-     * @return void
      */
     public function handle()
     {
         $url = $this->arguments()->getOption('url', null);
 
         if (!$url) {
+            $this->writeln('No --url parameter set, exiting!');
             return $this->exitWithError();
         }
 
+        if (!$this->checkIntervalHasPassed($this->config->get('tripwire_ignore_interval', 300))) {
+            $this->exitWithSuccess(); // Interval has not passed, don't send another HTTP request... yet!
+        }
+
         $this->writeln('Sending tripwire hook to: ' . $url);
+        $this->sendHttpWebhookRequest($this->config->get('tripwire_url', null), $this->config->get('tripwire_request_timeout', 30));
 
-        // @todo Add code here to check if an optional interval has passed since the last HTTP hook request.
-        // @todo Add code here to send the web hook.
-
+        touch($this->basePath . self::INTERVAL_CACHE_FILE);
         $this->exitWithSuccess();
+    }
+
+    /**
+     * Checks if the tripwire request TTL has passed (indicating we should now initiate a new web hook request)
+     * @param int $ttl The number of seconds that define the request interval.
+     * @return bool
+     */
+    private function checkIntervalHasPassed($ttl): bool
+    {
+        if (!file_exists($this->basePath . self::INTERVAL_CACHE_FILE)) {
+            return false;
+        }
+        if (filemtime($this->basePath . self::INTERVAL_CACHE_FILE) > (time() - $ttl)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Send the HTTP payload to the "Tripwire" endpoint.
+     * @param string $url The URL to send the JSON payload to.
+     * @param int $timeout The connection timeout value.
+     * @return bool
+     */
+    private function sendHttpWebhookRequest($url, $timeout = 10): bool
+    {
+        $ch = curl_init($url);
+        $payload = json_encode([
+            'action' => 'tripwire.notify',
+            'hostname' => gethostname(),
+            'timestamp' => date('c'),
+        ]);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $timeout + 2);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        if ($result) {
+            return true;
+        }
+        return false;
     }
 }
