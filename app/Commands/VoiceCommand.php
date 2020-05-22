5d3aa399fd7d303cf8c55c6a149edb4515ab2f5d
Bobby Allen <ballen@bobbyallen.me>
2020-05-22 16:12:29 +0100

    Pushing some tweaks to counteract the cutting of end of transmissions caused by the killing of the SOX process and what appears to be the fact that the SOX cannot write the last part of the message to disk before it's killed.

--- app/Commands/VoiceCommand.php
+++ app/Commands/VoiceCommand.php
@@ -177,7 +177,8 @@ class VoiceCommand extends AudioCommand implements CommandInterface
 
                 usleep(10000);
                 if ($timeout < microtime(true)) {
-                    system('kill ' . $pid);
+                    sleep(1); // Ensures that the EoT is not cut from the recording and gives SOX time to write to the disk before the kernel kills the process.
+                    system('kill -2 ' . $pid);
                     $this->outputLedRx->setValue(GPIO::LOW);
 
                     $this->storeRecording();
