ca6be0263026794e7989c28099d94c2925c1820d
Bobby Allen <ballen@bobbyallen.me>
2020-05-28 09:37:57 +0100

    Removing the 1s delay prior to PTT release, probably better to prioritise snappy re-transmission as opposed to slightly cutting off the end of recordings.

--- app/Commands/VoiceCommand.php
+++ app/Commands/VoiceCommand.php
@@ -207,7 +207,7 @@ class VoiceCommand extends AudioCommand implements CommandInterface
 
                     $this->outputLedRx->setValue(GPIO::LOW);
 
-                    sleep(1); // Ensures that the EoT is not cut from the recording and gives SOX time to write to the disk before the kernel kills the process.
+                    //sleep(1); // Ensures that the EoT is not cut from the recording and gives SOX time to write to the disk before the kernel kills the process.
                     system('kill -2 ' . $pid);
 
                     $this->storeRecording();
@@ -265,7 +265,7 @@ class VoiceCommand extends AudioCommand implements CommandInterface
 
                     $this->outputLedRx->setValue(GPIO::LOW);
 
-                    sleep(1); // Ensures that the EoT is not cut from the recording and gives SOX time to write to the disk before the kernel kills the process.
+                    //sleep(1); // Ensures that the EoT is not cut from the recording and gives SOX time to write to the disk before the kernel kills the process.
                     system('kill -2 ' . $pid);
 
                     $this->sendCourtesyTone();
