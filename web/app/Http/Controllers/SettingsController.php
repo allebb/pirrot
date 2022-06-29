345bd88f9a115461b17bdde1aafa9bc6dd0efa04
Bobby Allen <ballen@bobbyallen.me>
2022-06-29 19:21:58 +0100

    Adding new VOX sensitivity (fine tuning) parameters.

--- web/app/Http/Controllers/SettingsController.php
+++ web/app/Http/Controllers/SettingsController.php
@@ -184,8 +184,9 @@ class SettingsController extends Controller
             'The default value is "0.2" (if set to "0" there will be no delay and therefore immediately repeat the transmission which might lead to cut audio recordings)',
         ],
         'vox_tuning' => [
-            'You can fine tune your VOX levels if you require without having to adjust your mic/line-in volume. This is considered an advanced setting and one that you should experiment with if the default values do not work for your setup',
-            'Default value: "1 0.5 5% 1 1.0 5%" (eg. Wait until it hears activity above the threshold (5%) for half a second (0.5) then start recording, stop recording when audible activity falls below the threshold (5%) to zero for one second (1.0).',
+            'If you are using VOX mode, you can optionally fine tune your VOX levels as you require independently of having to adjust your mic/line-in volume.',
+            '**This is considered an advanced setting and one that you should experiment with if the default values do not work for your setup!**',
+            'Default value: "1 0.5 5% 1 1.0 5%"  - In this example, wait until it hears activity above the threshold (5%) for half a second (0.5) then start recording, stop recording when audible activity falls below the threshold (5%) to zero for one second (1.0).',
         ],
         'courtesy_tone' => [
             'To disable courtesy tones set to: false',
